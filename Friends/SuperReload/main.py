8770cd6087a46609e32c9d24493fa04dbdecd8e4
PengCheng <1932554894@qq.com>
2019-03-18 18:09:01 +0800

    Fix  super reload error

--- Friends/SuperReload/main.py
+++ Friends/SuperReload/main.py
@@ -5,5 +5,5 @@ def main():
     if os.path.isfile(st.DATA_FILE_PATH):
         with open(st.DATA_FILE_PATH, "rb") as opf:
             ks = cPickle.load(opf)
-        for k in iter(ks):
+        for k in iter(set(sys.modules.keys()) - set(ks)):
             del sys.modules[k]
